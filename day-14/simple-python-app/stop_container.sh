6a0c5eb493ccb17459c4464cfc4c6858f95f18c0
Akshay Burungale <126006109+AKSHAYBURUNGAL@users.noreply.github.com>
2024-10-02 20:24:46 +0530

    Update stop_container.sh

--- day-14/simple-python-app/stop_container.sh
+++ day-14/simple-python-app/stop_container.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 set -e
 
-# Stop the running container
-containerID=$(docker ps -q --filter ancestor=admin335/simple-python-flask-app)
-if [ -n "$containerID" ]; then
-  docker rm -f $containerID
-  echo "Stopped and removed container: $containerID"
-else
-  echo "No running container found for admin335/simple-python-flask-app."
-fi
+# Stop the running container (if any)
+echo "Hi"
