dcefc00d46ecaad400ec72e232b1b47e14e6fa5a
Akshay Burungale <126006109+AKSHAYBURUNGAL@users.noreply.github.com>
2024-10-02 20:24:13 +0530

    Update start_container.sh

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 set -e
 
-# Pull the Docker image from Docker Hub
-docker pull admin335/simple-python-flask-app
-
-# Run the Docker image as a container
-docker run -d -p 5000:5000 admin335/simple-python-flask-app
+# Stop the running container (if any)
+echo "Hi"
